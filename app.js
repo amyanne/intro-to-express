b16bf47b2b160a0ea2738366f5d3131582cf3931
Amy Westcott <amy.westcott@yahoo.com>
2020-08-24 20:04:38 -0600

    done

--- app.js
+++ app.js
@@ -7,35 +7,16 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+const http = require('http');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+let app = http.createServer((req, res) => {
+  // Set a response type of plain text for the response
+  res.writeHead(200, {'Content-Type': 'text/plain'});
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+  // Send back a response and end the connection
+  res.end('Hello World!\n');
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.listen(3000, '127.0.0.1');
+console.log('Node server running on port 3000');
 
 module.exports = app;
